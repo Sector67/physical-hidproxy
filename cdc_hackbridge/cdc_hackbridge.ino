37d8e77bb2869ef16aa375c74cb61150196c05a1
Liam M <archimedespi3141@gmail.com>
2014-09-06 17:30:42 -0500

    More code

--- cdc_hackbridge/cdc_hackbridge.ino
+++ cdc_hackbridge/cdc_hackbridge.ino
@@ -1,1 +1,14 @@
+#include <SoftwareSerial.h>
 
+#define USE_SOFTSERIAL
+#define SS_RX 14
+#define SS_TX 16
+
+SoftwareSerial toAndroid(SS_RX, SS_TX);
+
+void setup() {
+  Keyboard.begin();
+  
+  toAndroid.begin(9600);
+  toAndroid.println("Begun");
+}
