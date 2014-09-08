5120c74d4d2003a956692df73f77ae7f523d952b
Liam M <archimedespi3141@gmail.com>
2014-09-07 19:22:16 -0500

    New(er) code

---
+++ ftdi_to_hid/ftdi_to_hid.ino
@@ -0,0 +1,18 @@
+void setup() {
+   // open the serial port. Note that it is Serial1 and not just Serial
+   Serial1.begin(38400);
+   //wait for the serial port to become available
+   while (!Serial1){;}
+   //start the keyboard
+   Keyboard.begin();
+}
+
+void loop() {
+   // check for incoming serial data:
+   if (Serial1.available() > 0) {
+     // read incoming serial data:
+     char inChar = Serial1.read();
+     // Send that character to the keyboard:
+     Keyboard.write(inChar);
+   }
+}
